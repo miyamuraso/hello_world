7a98baade7e0a7984394e0cca230b68135345859
So Miyamura <miyamuraso@Miyamura-So-MacBook-Pro.local>
2018-12-11 13:54:53 +0900

    add form

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 document.getElementById("text-button").onclick = function() {
     document.getElementById("text").innerHTML = "クリックされた！";
   };
-  document.getElementById("square-button").onclick = function() {
+
+document.getElementById("square-button").onclick = function() {
     this.classList.toggle("blue");
   };
-    
+
+document.getElementById("form-button").onclick = function() {
+    document.getElementById("form-text").innerHTML = "こんにちは " + document.getElementById("name").value + " さん！";
+  };
