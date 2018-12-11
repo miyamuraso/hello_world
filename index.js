c699b5f2e38b9b0fba5add649ad290122a634119
So Miyamura <miyamuraso@Miyamura-So-MacBook-Pro.local>
2018-12-11 13:50:22 +0900

    add box

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 document.getElementById("text-button").onclick = function() {
     document.getElementById("text").innerHTML = "クリックされた！";
   };
-  
+  document.getElementById("square-button").onclick = function() {
+    this.classList.toggle("blue");
+  };
+    
