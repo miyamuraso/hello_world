054374d507782cde738db06986e8d9241f519164
So Miyamura <miyamuraso@Miyamura-So-MacBook-Pro.local>
2018-12-12 15:36:28 +0900

    npm install

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-document.getElementById("text-button").onclick = function() {
-    document.getElementById("text").innerHTML = "クリックされた！";
-  };
+// document.getElementById("text-button").onclick = function() {
+//     document.getElementById("text").innerHTML = "クリックされた！";
+//   };
 
-document.getElementById("square-button").onclick = function() {
-    this.classList.toggle("blue");
-  };
+// document.getElementById("square-button").onclick = function() {
+//     this.classList.toggle("blue");
+//   };
 
-document.getElementById("form-button").onclick = function() {
-    document.getElementById("form-text").innerHTML = "こんにちは " + document.getElementById("name").value + " さん！";
-  };
+// document.getElementById("form-button").onclick = function() {
+//     document.getElementById("form-text").innerHTML = "こんにちは " + document.getElementById("name").value + " さん！";
+//   };
+
+console.log("hello")
